41642f94ecbdf5c8ff556c52f729b4b2fbfb0b62
Cristian Ferreira (SknZ) <cristian.datingaa@gmail.com>
2020-06-12 01:04:17 -0300

    Resolvido comflito no Comando "ou!search"

--- src/main/java/me/skiincraft/discord/ousu/commands/SearchCommand.java
+++ src/main/java/me/skiincraft/discord/ousu/commands/SearchCommand.java
@@ -19,6 +19,7 @@ import me.skiincraft.discord.ousu.search.JSoupGetters;
 import me.skiincraft.discord.ousu.utils.ReactionMessage;
 import me.skiincraft.discord.ousu.utils.StringUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class SearchCommand extends Commands {
@@ -46,53 +47,64 @@ public class SearchCommand extends Commands {
 		if (args.length >= 1) {
 			List<BeatmapSearch> bb = new ArrayList<BeatmapSearch>();
 			List<EmbedBuilder> emb = new ArrayList<EmbedBuilder>();
-			sendEmbedMessage(TypeEmbed.LoadingEmbed().setTitle("Looking for Beatmaps")).queue(message -> {
-				GoogleSearch searchBearmap = null;
+			List<Message> mess = new ArrayList<Message>();
+			sendEmbedMessage(TypeEmbed.LoadingEmbed().setTitle("Looking for Beatmaps")).queue(m -> {
+				mess.add(m);
+			});
+			
+			while (mess.size() == 0) {
 				try {
-					searchBearmap = new GoogleSearch(StringUtils.arrayToString2(0, args));
-					List<Integer> beatmaplist = searchBearmap.getBeatmapSetIDs();
-					int i = 0;
-					for (int n : beatmaplist) {
-						bb.add(JSoupGetters.beatmapInfo(n));
-						if (bb.get(i) == null) {
-							continue;
-						}
-						emb.add(SearchEmbed.searchEmbed(bb.get(i), channel.getGuild()));
-						i++;
+					Thread.sleep(200L);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			Message message = mess.get(0);
+			
+			GoogleSearch searchBearmap = null;
+			try {
+				searchBearmap = new GoogleSearch(StringUtils.arrayToString2(0, args));
+				List<Integer> beatmaplist = searchBearmap.getBeatmapSetIDs();
+				int i = 0;
+				for (int n : beatmaplist) {
+					bb.add(JSoupGetters.beatmapInfo(n));
+					if (bb.get(i) == null) {
+						continue;
 					}
-
-					if (bb.contains(null)) {
-						bb.remove(null);
-					}
-
-					if (bb.size() == 0) {
-						throw new SearchNotFoundException("Não foi encontrado beatmap sem search");
-					}
-				} catch (SearchNotFoundException | InvalidBeatmapException | IOException e) {
-					String[] str = getLang().translatedArrayOsuMessages("INEXISTENT_BEATMAPID");
-					message.editMessage(TypeEmbed.WarningEmbed(str[0], StringUtils.commandMessage(str)).build()).queue();
-					return;
+					emb.add(SearchEmbed.searchEmbed(bb.get(i), channel.getGuild()));
+					i++;
 				}
 
-				message.editMessage(emb.get(0).build()).queue(message2 -> {
-					try {
-						channel.sendFile(bb.get(0).getBeatmapPreview(), bb.get(0).getTitle() + ".mp3").queue();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+				if (bb.contains(null)) {
+					bb.remove(null);
+				}
 
-					EmbedBuilder[] bm = new EmbedBuilder[emb.size()];
-					emb.toArray(bm);
-					
-					ReactionMessage.searchReactions.add(new DefaultReaction(getUserId(), message.getId(), bm, 0));
-					// message2.addReaction("U+1F3AF").queue();
-					message2.addReaction("U+25C0").queue();
-					message2.addReaction("U+25B6").queue();
+				if (bb.size() == 0) {
+					throw new SearchNotFoundException("Não foi encontrado beatmap sem search");
+				}
+			} catch (SearchNotFoundException | InvalidBeatmapException | IOException e) {
+				String[] str = getLang().translatedArrayOsuMessages("INEXISTENT_BEATMAPID");
+				message.editMessage(TypeEmbed.WarningEmbed(str[0], StringUtils.commandMessage(str)).build()).queue();
+				return;
+			}
 
-				});
+			message.editMessage(emb.get(0).build()).queue(message2 -> {
+				try {
+					channel.sendFile(bb.get(0).getBeatmapPreview(), bb.get(0).getTitle() + ".mp3").queue();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				EmbedBuilder[] bm = new EmbedBuilder[emb.size()];
+				emb.toArray(bm);
+				
+				ReactionMessage.searchReactions.add(new DefaultReaction(getUserId(), message.getId(), bm, 0));
+				// message2.addReaction("U+1F3AF").queue();
+				message2.addReaction("U+25C0").queue();
+				message2.addReaction("U+25B6").queue();
+
 			});
-
-			return;
 		}
 
 	}
