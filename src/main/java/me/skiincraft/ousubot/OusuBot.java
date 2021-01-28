269a1f6de0096885a4cef5b6960cdcf1c98fbbea
Cristian "SknZ" Ferreira <61399406+Cristian-Sknz@users.noreply.github.com>
2021-01-28 19:46:21 -0300

    [feature] DiscordBotListAPI funcionando com token no classpath.

--- src/main/java/me/skiincraft/ousubot/OusuBot.java
+++ src/main/java/me/skiincraft/ousubot/OusuBot.java
@@ -3,6 +3,7 @@ package me.skiincraft.ousubot;
 import me.skiincraft.beans.Injector;
 import me.skiincraft.beans.annotation.Inject;
 import me.skiincraft.discord.core.OusuCore;
+import me.skiincraft.discord.core.common.PresenceUpdater;
 import me.skiincraft.discord.core.common.reactions.ReactionListeners;
 import me.skiincraft.discord.core.common.reactions.Reactions;
 import me.skiincraft.discord.core.language.Language;
@@ -11,33 +12,47 @@ import me.skiincraft.ousubot.api.DiscordBotAPI;
 import me.skiincraft.ousubot.api.OusuAPI;
 import me.skiincraft.ousubot.view.emotes.GenericsEmotes;
 import net.dv8tion.jda.api.Region;
+import net.dv8tion.jda.api.entities.Activity;
 
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class OusuBot extends OusuPlugin {
 
     @Inject
     private GenericsEmotes emotes;
+    private static PresenceUpdater presenceUpdater;
 
     @Override
     public void onEnable() {
         OusuCore.addLanguage(new Language(new Locale("pt", "BR"), Region.BRAZIL));
         OusuCore.addLanguage(new Language(new Locale("en", "US")));
-        injectAPI(new OusuAPI(), null); //new DiscordBotAPI());
+        injectAPI(new OusuAPI(), new DiscordBotAPI());
         emotes.loadEmotes(OusuCore.getAssetsPath().toAbsolutePath() + "/emotes/");
 
         ReactionListeners reactionListeners = new ReactionListeners();
         Reactions reactions = new Reactions(reactionListeners);
         OusuCore.registerListener(reactionListeners);
 
+        presenceUpdater = new PresenceUpdater(Arrays.asList(Activity.listening("ou!help for help."),
+                Activity.watching("{guildsize} Servidores online."),
+                Activity.listening("Ousubot has been improved and is better!")));
+
+        Executors.newSingleThreadScheduledExecutor()
+                .scheduleAtFixedRate(OusuCore::shutdown, 1,1, TimeUnit.HOURS);
+    }
+
+    public static PresenceUpdater getPresenceUpdater() {
+        return presenceUpdater;
     }
 
     private void injectAPI(OusuAPI api, DiscordBotAPI discordBotAPI){
         Injector injector = OusuCore.getInjector();
         injector.inject(api);
-        //injector.inject(discordBotAPI);
         injector.map(api);
-        //injector.map(discordBotAPI);
+        injector.map(discordBotAPI);
         api.setup();
     }
 }
--- src/main/java/me/skiincraft/ousubot/api/DiscordBotAPI.java
+++ src/main/java/me/skiincraft/ousubot/api/DiscordBotAPI.java
@@ -1,28 +1,25 @@
 package me.skiincraft.ousubot.api;
 
-import me.skiincraft.beans.annotation.Component;
-import me.skiincraft.beans.annotation.Inject;
+import com.google.gson.JsonParser;
 import me.skiincraft.discord.core.OusuCore;
+import me.skiincraft.ousubot.OusuBot;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import org.discordbots.api.client.DiscordBotListAPI;
 import org.discordbots.api.client.impl.DiscordBotListAPIImpl;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class DiscordBotAPI {
 
-    @Inject
-    private ShardManager shardManager;
     private DiscordBotListAPI api;
 
     public DiscordBotAPI(){
-        this.api = new DiscordBotListAPIImpl(getToken(), shardManager.getShards().get(0).getSelfUser().getId());
+        this.api = new DiscordBotListAPIImpl(getToken(), OusuCore.getShardManager().getShards().get(0).getSelfUser().getId());
     }
 
     public void updateStats(){
-        api.setStats(shardManager.getShards()
+        api.setStats(OusuCore.getShardManager().getShards()
                 .stream()
                 .map(jda -> Integer.parseInt("" + jda.getGuildCache().size()))
                 .collect(Collectors.toList()));
@@ -33,14 +30,13 @@ public class DiscordBotAPI {
     }
 
     public ShardManager getShardManager() {
-        return shardManager;
+        return OusuCore.getShardManager();
     }
 
     public String getToken() {
-        try {
-            return new String(Files.readAllBytes(Paths.get(OusuCore.getAssetsPath().toFile().getAbsolutePath() + "/dbl-token.txt")));
-        } catch (Exception e){
-            throw new RuntimeException("Não foi possível conectar a API do DiscordBotList");
-        }
+        return new JsonParser().parse(new InputStreamReader(OusuBot.class.getResourceAsStream("/DBLToken.json")))
+                .getAsJsonObject()
+                .get("key")
+                .getAsString();
     }
 }
